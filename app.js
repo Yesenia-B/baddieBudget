14c5cec9f950cb6bc31a51b8a687528e40b9adfa
Yesenia <burgosy@sunypoly.edu>
2026-04-29 18:49:42 -0400

    gave project update, working on documentation, chart.js, and display

--- app.js
+++ app.js
@@ -12,3 +12,27 @@ if (menu && menuLinks) {
   });
 }
 });
+
+if(!modal || !openBtn || !closeBtn) {
+  console.log("Modal Elements missing!");
+  return;
+}
+
+const modal = document.getElementById("transaction-modal");
+const openBtn = document.getElementById("open-modal-btn");
+const closeBtn = document.getElementById(".close-btn");
+
+openBtn.addEventListener("submit", () => {
+  modal.style.display = "block";
+});
+
+close-openBtn. addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if(e.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
